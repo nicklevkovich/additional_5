0ac4164d05683fb5a1ccd2c112f61e1f8a61352c
Nick Levkovich <nicklevkovic@gmail.com>
2018-09-20 01:56:04 +0300

    11/20 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var a = str.split("");
+var al = a.length;
+for (i=0; i<al;i++) {
+  for (j=0; j<al;j++) {
+    if ((a[j]==='(')&&(a[j+1]===')')) a.splice(j,2);
+  }
+}
+ if (a.length===0) return(1)
+  else return(0);
 }
