a21a28eac40605c433c1a0b422f008757dd9f4e9
Nick Levkovich <nicklevkovic@gmail.com>
2018-09-20 02:15:16 +0300

    Final solution - 20/20 passing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
 var a = str.split("");
 var al = a.length;
-for (i=0; i<al;i++) {
+for (i=0; i<(2*al);i++) {
   for (j=0; j<al;j++) {
-    if ((a[j]==='(')&&(a[j+1]===')')) a.splice(j,2);
+    if (((a[j]==='(')&&(a[j+1]===')'))
+      ||((a[j]==='[')&&(a[j+1]===']'))
+      ||((a[j]==='{')&&(a[j+1]==='}'))
+      ||((a[j]==='1')&&(a[j+1]==='2'))
+      ||((a[j]==='3')&&(a[j+1]==='4'))
+      ||((a[j]==='5')&&(a[j+1]==='6'))
+      ||((a[j]==='7')&&(a[j+1]==='7'))
+      ||((a[j]==='8')&&(a[j+1]==='8'))
+      ||((a[j]==='|')&&(a[j+1]==='|'))) a.splice(j,2);
   }
 }
+
  if (a.length===0) return(1)
   else return(0);
 }
